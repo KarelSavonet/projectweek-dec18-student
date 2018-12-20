afe7dd327a603519024d95c2cac219fa4eb37e25
KarelSavonet <43341133+KarelSavonet@users.noreply.github.com>
2018-12-20 14:47:51 +0100

    horizontalChain werkt niet helemaal?

--- p5r/empty-example/sketch.js
+++ p5r/empty-example/sketch.js
@@ -24,8 +24,9 @@ function setup() {
 	green = color(0,255,0);
 	red = color(255,0,0);
 	orange = color (255,128,0);
+	purple = color(128,0,128);
 	leeg = color(255,255,255);
-	kleuren = [blue,yellow,green,red,orange];
+	kleuren = [blue,yellow,green,red,orange,purple];
 	aantalKleuren = kleuren.length;
     createCanvas(600,600);
     background(200);
@@ -98,19 +99,47 @@ function isBuur(){
 	let b = grid[vak2.rij][vak2.kolom];
     let horizontaleBuur = (a.position.x+1 === b.position.x || a.position.x -1 === b.position.x) && (a.position.y === b.position.y);
     let verticaleBuur = (a.position.y+1 === b.position.y || a.position.y -1 === b.position.y) && (a.position.x === b.position.x);
-
     return (horizontaleBuur || verticaleBuur);
 }
 
 function swap(){
 	if (isBuur()){
-		let temp = grid[vak1.rij][vak1.kolom];
-		grid[vak1.rij][vak1.kolom] = grid[vak2.rij][vak2.kolom];
-		grid[vak2.rij][vak2.kolom] = temp;
-
+		let temp = grid[vak1.rij][vak1.kolom].kleur;
+		grid[vak1.rij][vak1.kolom].kleur = grid[vak2.rij][vak2.kolom].kleur;
+		grid[vak2.rij][vak2.kolom].kleur = temp;
+		console.log(horizontalChainAt(vak1));
 	}
 	grid[vak1.rij][vak1.kolom].actief = false;
 	grid[vak2.rij][vak2.kolom].actief = false;
 	vak1 = null;
 	vak2 = null;
+	background(200);
+}
+
+
+function breedte(){
+	return grid[0].length;
+}
+
+function hoogte(){
+	return grid.length;
+}
+
+function horizontalChainAt(vak){
+    let kleurVak = grid[vak.rij][vak.kolom].kleur;
+    let ketting = 1;
+    let i = 1;
+
+    while(vak.kolom+i < breedte() && grid[vak.rij][vak.kolom+i].kleur === kleurVak){
+    	ketting++;
+    	i++;
+	}
+
+
+	while(vak.kolom-i >= 0 && grid[vak.rij][vak.kolom-i].kleur === kleurVak){
+    	ketting++;
+    	i++;
+	}
+
+    return ketting;
 }
